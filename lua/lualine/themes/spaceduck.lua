6834752d445f41b9bb101155c260fcfe8be64e1c
Kyle Kent <kyle.kent321@gmail.com>
2024-10-25 12:36:00 -0500

    more custom configs

--- lua/lualine/themes/spaceduck.lua
+++ lua/lualine/themes/spaceduck.lua
@@ -11,7 +11,9 @@ local colors = {
   gray = '#686f9a',
   darkgray = '#30365F',
   lightgray = '#c1c3cc',
-  orange='#e39400'
+  orange='#e39400',
+  visual_selection='#1b1c36',
+  cursor_line='#16172d'
 }
 
 spaceduck.normal = {
@@ -24,39 +26,44 @@ spaceduck.normal = {
 
 spaceduck.insert = {
   a = {bg = colors.orange, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.darkgray, fg = colors.lightgray},
+  b = {bg = colors.visual_selection, fg = colors.lightgray},
   c = {bg = colors.black, fg = colors.lightgray}
 }
 
 spaceduck.visual = {
   a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.darkgray, fg = colors.lightgray},
+  b = {bg = colors.visual_selection, fg = colors.lightgray},
   c = {bg = colors.black, fg = colors.lightgray}
 }
 
 spaceduck.replace = {
   a = {bg = colors.purple, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.darkgray, fg = colors.lightgray},
+  b = {bg = colors.visual_selection, fg = colors.lightgray},
   c = {bg = colors.black, fg = colors.lightgray}
 }
 
 spaceduck.command = {
   a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.darkgray, fg = colors.lightgray},
+  b = {bg = colors.visual_selection, fg = colors.lightgray},
   c = {bg = colors.black, fg = colors.lightgray}
 }
 
 spaceduck.terminal = {
   a = {bg = colors.orange, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.darkgray, fg = colors.lightgray},
+  b = {bg = colors.visual_selection, fg = colors.lightgray},
   c = {bg = colors.black, fg = colors.lightgray}
 }
 
 spaceduck.inactive = {
-  a = {bg = colors.black, fg = colors.lightgray},
-  c = {bg = colors.darkgray, fg = colors.lightgray},
-  z = {bg = colors.black, fg = colors.lightgray}
+  a = {bg = colors.darkgray, fg = colors.lightgray},
+  c = {bg = colors.visual_selection, fg = colors.purple},
+  z = {bg = colors.darkgray, fg = colors.lightgray}
+}
+
+spaceduck.options = {
+    section_s
 }
 
 -- lualine.theme = spaceduck
 return spaceduck
+
